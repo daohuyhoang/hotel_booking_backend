c2cc75ec99403f4c6fcf6dec6f71fa5d05da00cc
daohuyhoang <daohuyhoang21082000@gmail.com>
2024-10-10 20:15:00 +0700

    update dto hotel, room

--- src/main/java/com/daisy/daisy_hotel_backend/dto/request/HotelDTO.java
+++ src/main/java/com/daisy/daisy_hotel_backend/dto/request/HotelDTO.java
@@ -1,5 +1,8 @@
 package com.daisy.daisy_hotel_backend.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,8 +15,18 @@ import lombok.Setter;
 public class HotelDTO {
 
     private Long hotelId;
+
+    @NotBlank(message = "Hotel name cannot be blank")
+    @Size(max = 100, message = "Hotel name must not exceed 100 characters")
     private String name;
+
+    @Size(max = 255, message = "Address must not exceed 255 characters")
     private String address;
+
+    @Size(min = 10, max = 10, message = "Phone number must be exactly 10 digits")
     private String phoneNumber;
+
+    @Email(message = "Email should be valid")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
     private String email;
 }
--- src/main/java/com/daisy/daisy_hotel_backend/dto/request/RoomDTO.java
+++ src/main/java/com/daisy/daisy_hotel_backend/dto/request/RoomDTO.java
@@ -1,9 +1,8 @@
 package com.daisy.daisy_hotel_backend.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.daisy.daisy_hotel_backend.model.enums.RoomStatus;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -13,9 +12,26 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class RoomDTO {
     private Long roomId;
+
+    @NotBlank(message = "Room number must be not blank")
+    @Size(max = 20, message = "Room number must not exceed 20 characters")
     private String roomNumber;
+
+    @NotBlank(message = "Room type cannot be blank")
+    @Size(max = 100, message = "Room type must not exceed 100 characters")
     private String type;
+
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.00", message = "Price must be greater than or equal to 0.00")
+    @DecimalMax(value = "9999999999.99", message = "Price must be less than or equal to 9999999999.99")
     private BigDecimal price;
+
+    @Min(value = 1, message = "Capacity must be at least 1")
     private Integer capacity;
+
+    @NotBlank(message = "Description cannot be null")
     private String description;
+
+    @NotNull(message = "Availability status cannot be null")
+    private RoomStatus availabilityStatus;
 }
